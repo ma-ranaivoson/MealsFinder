b121011eaf7a3cc15947e347bac80fae31f8abec
ma <rv.nirmalala@gmail.com>
2021-12-30 13:09:15 +0300

    Update: App.tsx -> Adding & configuring expo-google-font Update: RestaurantsInfoCard -> finishing card design

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+// eslint-disable-next-line camelcase
+import { useFonts, Oswald_400Regular } from '@expo-google-fonts/oswald';
+// eslint-disable-next-line camelcase
+import { Lato_400Regular } from '@expo-google-fonts/lato';
 import { ThemeProvider } from 'styled-components/native';
 import RestaurantsScreen from './src/features/restaurants/screens/RestaurantsScreen';
 
 import theme from './src/infrastructure/theme/theme';
 
 export default function App() {
+  const [oswaldLoaded] = useFonts({
+    // eslint-disable-next-line camelcase
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useFonts({
+    // eslint-disable-next-line camelcase
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded && !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
--- src/features/restaurants/components/RestaurantsInfoCard.tsx
+++ src/features/restaurants/components/RestaurantsInfoCard.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { Image, Text } from 'react-native';
 import { Card } from 'react-native-paper';
 import styled from 'styled-components/native';
+import { SvgXml } from 'react-native-svg';
+import star from '../../../../assets/star';
+import open from '../../../../assets/open';
 
 interface Restaurant {
   name: string;
@@ -21,13 +25,37 @@ const RestaurantCard = styled(Card)`
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
-  padding: 20px;
+  padding: ${({ theme }) => theme.Spacing.space[3]};
   background-color: white;
 `;
 
 const Title = styled.Text`
-  padding: 16px;
-  color: ${(props) => props.theme.Color.text.error};
+  font-family: ${({ theme }) => theme.Font.fonts.heading};
+  font-size: ${({ theme }) => theme.Font.fontSizes.body};
+  color: ${(props) => props.theme.Color.text.primary};
+`;
+
+const Info = styled.View`
+  padding: ${({ theme }) => theme.Spacing.space[3]};
+`;
+
+const Address = styled.Text`
+  font-family: ${({ theme }) => theme.Font.fonts.body};
+  font-size: ${({ theme }) => theme.Font.fontSizes.caption};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${({ theme }) => theme.Spacing.space[2]};
+  padding-bottom: ${({ theme }) => theme.Spacing.space[2]};
+  justify-content: space-between;
+`;
+
+const Rate = styled.View`
+  flex-direction: row;
+`;
+const Open = styled.View`
+  flex-direction: row;
 `;
 
 export default function RestaurantInfoCard({
@@ -35,20 +63,50 @@ export default function RestaurantInfoCard({
 }: Props) {
   const {
     name = 'Some Restaurant',
-    icon,
+    icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
     photos = [
       'https://media.gettyimages.com/photos/cozy-restaurant-for-gathering-with-friends-picture-id1159992039?s=612x612',
     ],
-    address,
-    isClosedTemporarly,
-    isOpenNow = true,
-    rating = 4,
+    address = '100 Some random street',
+    isClosedTemporarly = true,
+    isOpenNow = false,
+    rating = 5,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <RestaurantCard elevation={6}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Rating>
+          <Rate>
+            {ratingArray.map((elem) => (
+              <SvgXml key={elem} xml={star} width={20} height={20} />
+            ))}
+          </Rate>
+          <Open>
+            {isClosedTemporarly && (
+              <Text style={{ color: 'red', paddingLeft: 16, paddingRight: 16 }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            {isOpenNow && (
+              <SvgXml
+                xml={open}
+                width={20}
+                height={20}
+                style={{ paddingLeft: 16 }}
+              />
+            )}
+            {isClosedTemporarly && (
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            )}
+          </Open>
+        </Rating>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 }
