0634ff84e7f60e22654c335c73ed637a74f641d0
ma <rv.nirmalala@gmail.com>
2022-01-19 12:54:14 +0300

    Adding watermelon animation

--- src/components/utility/SafeArea.tsx
+++ src/components/utility/SafeArea.tsx
@@ -3,7 +3,8 @@ import { SafeAreaView, StatusBar } from 'react-native';
 
 const SafeArea = styled(SafeAreaView)`
     flex: 1;
-    ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}`}
+    ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}`};
+    background-color: ${(props) => props.theme.Color.bg.primary};
 `;
 
 export default SafeArea;
--- src/features/account/screens/AccountScreen.tsx
+++ src/features/account/screens/AccountScreen.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import LottieView from 'lottie-react-native';
+import styled from 'styled-components/native';
 import {
   AccountBackground,
   AccountContainer,
@@ -7,10 +9,27 @@ import {
   Title,
 } from '../components/AccountStyles';
 
+const AnimationWrapper = styled.View`
+  position: absolute;
+  width: 100%;
+  z-index: 999;
+  height: 40%;
+  top: 30px;
+`;
+
 export default function AccountScreen({ navigation }: { navigation: any }) {
   return (
     <AccountBackground>
       <AccountCover />
+      <AnimationWrapper>
+        <LottieView
+          autoPlay
+          loop
+          resizeMode="cover"
+        // eslint-disable-next-line global-require
+          source={require('../../../../assets/watermelon.json')}
+        />
+      </AnimationWrapper>
       <Title>Meals Finder</Title>
       <AccountContainer>
         <AuthButton
