358f9f2828f37ef2b14c82f4c167280ff5fc03df
ma <rv.nirmalala@gmail.com>
2022-01-21 22:11:52 +0300

    Adding firebase routes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
+import { geocodeRequest } from './geocode';
 
 // eslint-disable-next-line import/prefer-default-export
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
+export const geocode = functions.https.onRequest((request, response) => {
+  geocodeRequest(request, response);
 });
